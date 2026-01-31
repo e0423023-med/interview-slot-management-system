2b416ba99786274cb40767fbbd22d585d6744c6b
e0423023-med <e0423023@sriher.edu.in>
2026-01-31 15:13:29 +0530

    added start script

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 module.exports = () => {
     mongoose
-    .connect("mongodb://localhost:27017/test-rms-db")
+    .connect(process.env.DB_URL)
         .then(() => { 
             console.log("database connected") })
         .catch((err) => { console.log(err) })
