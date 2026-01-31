88bb34d97697d631ebf738df87d8cfba045563dd
e0423023-med <e0423023@sriher.edu.in>
2026-01-31 14:10:30 +0530

    added start script

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
+require ("dotenv").config()
 const mongoose = require("mongoose")
 
 module.exports = () => {
-    mongoose.connect("mongodb://localhost:27017/test-rms-db")
-        .then(() => { console.log("database connected") })
+    mongoose
+    .connect("mongodb://localhost:27017/test-rms-db")
+        .then(() => { 
+            console.log("database connected") })
         .catch((err) => { console.log(err) })
 }
--- models/userModel.js
+++ models/userModel.js
@@ -7,4 +7,4 @@ const userSchema = mongoose.Schema({
     password: String
 })
 const User = mongoose.model("User", userSchema)
-module.exports = User
+module.exports = User 
